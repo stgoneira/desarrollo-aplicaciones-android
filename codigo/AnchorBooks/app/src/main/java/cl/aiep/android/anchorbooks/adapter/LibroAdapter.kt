17cca233b54d5e7bc7225acae08bbd8b6226dad4
stgoneira <stgoneiran@gmail.com>
2022-06-15 01:04:34 -0400

    anchor books con ViewBinding

--- codigo/AnchorBooks/app/src/main/java/cl/aiep/android/anchorbooks/adapter/LibroAdapter.kt
+++ codigo/AnchorBooks/app/src/main/java/cl/aiep/android/anchorbooks/adapter/LibroAdapter.kt
@@ -1,46 +1,30 @@
 package cl.aiep.android.anchorbooks.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import cl.aiep.android.anchorbooks.R
 import cl.aiep.android.anchorbooks.databinding.LibroItemBinding
 import cl.aiep.android.anchorbooks.modelo.Libro
 import com.squareup.picasso.Picasso
 
 class LibroAdapter(private val datos:List<Libro>):RecyclerView.Adapter<LibroAdapter.ViewHolder>() {
 
-    class ViewHolder(view:View):RecyclerView.ViewHolder(view) {
-        //lateinit var binding: LibroItemBinding
-        var titulo:TextView
-        var imagen:ImageView
-
-        init {
-            titulo = view.findViewById(R.id.tvTitulo)
-            imagen = view.findViewById(R.id.imageView)
-            //binding = LibroItemBinding.inflate( LayoutInflater.from(view.context) )
-        }
-    }
+    class ViewHolder(val binding:LibroItemBinding):RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from( parent.context).inflate(R.layout.libro_item, parent, false)
-        return ViewHolder(view)
+        val binding = LibroItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val libro = datos.get(position)
-        holder.titulo.text = libro.titulo
-        Picasso.get().load(libro.imagen).into(holder.imagen)
-        /*
-        holder.binding.tvTitulo.text = libro.titulo
-        holder.binding.tvAutor.text = libro.autor
-        holder.binding.tvLenguaje.text = libro.lenguaje
-        holder.binding.tvPais.text = libro.pais
-        Picasso.get().load(libro.imagen).into(holder.binding.imageView)
-         */
+        with(holder.binding) {
+            tvTitulo.text   = libro.titulo
+            tvAutor.text    = "Autor: ${libro.autor}"
+            tvLenguaje.text = "Lenguaje: ${libro.lenguaje}"
+            tvPais.text     = "País: ${libro.pais}"
+            Picasso.get().load(libro.imagen).into(imageView)
+        }
     }
 
     override fun getItemCount(): Int {
